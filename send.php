356cc007d8e3b6e8c84b0a54189cbb08b2894b0a
Bethuel Correa <bethuelcorrea@gmail.com>
2019-01-16 22:30:36 -0600

    Update send.php

--- send.php
+++ send.php
@@ -11,7 +11,7 @@ header ("Location: sms_error.php");
 
 else if ($carrier == "verizon") {
 $formatted_number = $to."@vtext.com";
-mail("$formatted_number", "SMS", "$message"); 
+mail("$formatted_number", "LHM Poll: $from", "$message" , "$header"); 
 // Currently, the subject is set to "SMS". Feel free to change this.
 
 header ("Location: sms_success.php");
@@ -19,21 +19,21 @@ header ("Location: sms_success.php");
 
 else if ($carrier == "tmobile") {
 $formatted_number = $to."@tomomail.net";
-mail("$formatted_number", "SMS", "$message", "$header");
+mail("$formatted_number", "LHM Poll: $from", "$message" , "$header");
 
 header ("Location: sms_success.php");
 }
 
 else if ($carrier == "sprint") {
 $formatted_number = $to."@messaging.sprintpcs.com";
-mail("$formatted_number", "SMS", "$message");
+mail("$formatted_number", "LHM Poll: $from", "$message" , "$header");
 
 header ("Location: sms_success.php");
 }
 
 else if ($carrier == "att") {
 $formatted_number = $to."@txt.att.net";
-mail("$formatted_number", "SMS", "$message");
+mail("$formatted_number", "LHM Poll: $from", "$message" , "$header");
 header ("Location: sms_success.php");
 }
 
@@ -46,7 +46,7 @@ else if ($carrier == "metropcs") {
 
 else if ($carrier == "virgin") {
 $formatted_number = $to."@vmobl.com";
-mail("$formatted_number", "SMS", "$message");
+mail("$formatted_number", "LHM Poll: $from", "$message" , "$header");
 
 header ("Location: sms_success.php");
 }
